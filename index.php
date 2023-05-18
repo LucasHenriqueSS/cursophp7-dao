7c441f8fcf793da59b9888fc017e860c73a87d4e
LucasHenriqueSS <l.henriquess01@gmail.com>
2023-05-18 12:02:17 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,11 +2,27 @@
 
 require_once("config.php");
 
-$usuario1 = new Usuario();
-
+// CARREGA UM USUARIO ESPECIFICO
+/*$usuario1 = new Usuario();
 $usuario1->loadById(5);
+echo $usuario1;*/
 
-echo $usuario1;
+//CARREA UMA LISTA DE USUARIOS
+//$usuarios =  Usuario::getList();
+//echo json_encode($usuarios);
+
+
+// CARREGA UMA LISTA DE USUARIOS BUSCANDO PELO LOGIN
+//$search = Usuario::search("lu");
+//echo json_encode($search);
+
+
+$busca = new Usuario();
+
+$busca->login("lula", "bolsonaro");
+
+echo $busca;
+
 
 
 
